090b88d8cc62ccae0a5f4a0d7748be82795f482a
Ahmet Rasim Beyhan <ahmetrasimbayhan@gmail.com>
2025-08-13 11:28:26 +0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// For custom domain GitHub Pages (e.g., https://ahmetrasimbeyhan.online),
+// base must be '/'
 export default defineConfig({
   plugins: [react()],
-  base: '/ahmetrasimbeyhanportfolio/'
+  base: '/'
 });
 
 
